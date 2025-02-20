73ffa1e14f6ef9158ebcc69307aac06a28d9d0e6
Fahmousss <09031382227177@student.unsri.ac.id>
2025-02-21 01:13:40 +0700

    Adding semester into data

--- src/lib/dummy.ts
+++ src/lib/dummy.ts
@@ -1,6 +1,7 @@
 export interface Mahasiswa {
   id: number;
   nama: string;
+  semester: string;
   nim: string;
   prodi: string;
   angkatan: number;
@@ -27,6 +28,7 @@ export const dataFakultas: Fakultas[] = [
       {
         id: 101,
         nama: "Ahmad Setiawan",
+        semester: "1",
         nim: "22011001",
         prodi: "Teknik Informatika",
         angkatan: 2022,
@@ -41,6 +43,7 @@ export const dataFakultas: Fakultas[] = [
       {
         id: 102,
         nama: "Dina Lestari",
+        semester:"1",
         nim: "22011002",
         prodi: "Teknik Elektro",
         angkatan: 2022,
@@ -50,6 +53,21 @@ export const dataFakultas: Fakultas[] = [
           metode_pembayaran: null,
           jumlah_pembayaran: 0,
           waktu_pembayaran: null
+        }
+      },
+      {
+        id: 103,
+        nama: "Rizky Saputra",
+        semester:"2",
+        nim: "22011003",
+        prodi: "Teknik Mesin",
+        angkatan: 2022,
+        ukt: {
+          biaya: 7200000,
+          status_pembayaran: "Lunas",
+          metode_pembayaran: "Virtual Account",
+          jumlah_pembayaran: 7200000,
+          waktu_pembayaran: "2025-02-11 11:15:00"
         }
       }
     ]
@@ -61,6 +79,7 @@ export const dataFakultas: Fakultas[] = [
       {
         id: 201,
         nama: "Bayu Pratama",
+        semester:"1",
         nim: "22021001",
         prodi: "Manajemen",
         angkatan: 2022,
@@ -75,6 +94,7 @@ export const dataFakultas: Fakultas[] = [
       {
         id: 202,
         nama: "Citra Dewi",
+        semester:"1",
         nim: "22021002",
         prodi: "Akuntansi",
         angkatan: 2022,
@@ -84,6 +104,108 @@ export const dataFakultas: Fakultas[] = [
           metode_pembayaran: null,
           jumlah_pembayaran: 0,
           waktu_pembayaran: null
+        }
+      },
+      {
+        id: 203,
+        nama: "Indra Gunawan",
+        semester:"2",
+        nim: "22021003",
+        prodi: "Ekonomi Pembangunan",
+        angkatan: 2022,
+        ukt: {
+          biaya: 6800000,
+          status_pembayaran: "Lunas",
+          metode_pembayaran: "Transfer Bank",
+          jumlah_pembayaran: 6800000,
+          waktu_pembayaran: "2025-02-12 09:30:00"
+        }
+      }
+    ]
+  },
+  {
+    id: 3,
+    nama: "Fakultas Ilmu Komputer",
+    mahasiswa: [
+      {
+        id: 301,
+        nama: "Siti Rahma",
+        semester:"1",
+        nim: "22031001",
+        prodi: "Sistem Informasi",
+        angkatan: 2022,
+        ukt: {
+          biaya: 7300000,
+          status_pembayaran: "Belum Lunas",
+          metode_pembayaran: null,
+          jumlah_pembayaran: 0,
+          waktu_pembayaran: null
+        }
+      },
+      {
+        id: 302,
+        nama: "Fajar Maulana",
+        semester:"2",
+        nim: "22031002",
+        prodi: "Ilmu Komputer",
+        angkatan: 2022,
+        ukt: {
+          biaya: 7400000,
+          status_pembayaran: "Lunas",
+          metode_pembayaran: "E-Wallet",
+          jumlah_pembayaran: 7400000,
+          waktu_pembayaran: "2025-02-13 15:20:00"
+        }
+      }
+    ]
+  },
+  {
+    id: 4,
+    nama: "Fakultas Kedokteran",
+    mahasiswa: [
+      {
+        id: 401,
+        nama: "Anisa Putri",
+        semester:"1",
+        nim: "22041001",
+        prodi: "Pendidikan Dokter",
+        angkatan: 2022,
+        ukt: {
+          biaya: 15000000,
+          status_pembayaran: "Belum Lunas",
+          metode_pembayaran: null,
+          jumlah_pembayaran: 0,
+          waktu_pembayaran: null
+        }
+      },
+      {
+        id: 402,
+        nama: "Doni Saputra",
+        semester:"2",
+        nim: "22041002",
+        prodi: "Kedokteran Gigi",
+        angkatan: 2022,
+        ukt: {
+          biaya: 14000000,
+          status_pembayaran: "Lunas",
+          metode_pembayaran: "Transfer Bank",
+          jumlah_pembayaran: 14000000,
+          waktu_pembayaran: "2025-02-14 08:00:00"
+        }
+      },
+      {
+        id: 402,
+        nama: "Doni Saputra",
+        semester:"1",
+        nim: "22041002",
+        prodi: "Kedokteran Gigi",
+        angkatan: 2022,
+        ukt: {
+          biaya: 14000000,
+          status_pembayaran: "Lunas",
+          metode_pembayaran: "Transfer Bank",
+          jumlah_pembayaran: 14000000,
+          waktu_pembayaran: "2025-02-14 08:00:00"
         }
       }
     ]
